0b6dbae477aa0c1a80e8051f62c932ad701019ba
TAAT <20purpleblue10@gmail.com>
2023-01-12 09:34:33 +0900

    Initial Commit

---
+++ WebsocketTest/WebsocketTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketTestApp.swift
+//  WebsocketTest
+//
+//  Created by 董 亜飛 on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
